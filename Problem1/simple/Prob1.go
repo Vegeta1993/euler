0dbbbca2b8239a7ef46e9533475b6f7da5827d30
ahalsnad <halsna01@gw.uni-passau.de>
2018-12-28 11:31:56 +0100

    README added and descriptions for problems added

--- Problem1/simple/Prob1.go
+++ Problem1/simple/Prob1.go
@@ -1,8 +1,17 @@
+/*
+TITLE: Multiples of 3 and 5
+
+DESCRIPTION: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
+
 package main
 
 import (
 	"fmt"
 )
+
+// Calculates sum of numbers divisible by 3 and 5
 
 func Sum(n int) int {
 
--- Problem1/suggested/Prob11.go
+++ Problem1/suggested/Prob11.go
@@ -1,9 +1,20 @@
+/*
+TITLE:Multiples of 3 and 5:
+
+DESCRIPTION:If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+*/
 package main
 
 import (
 	"fmt"
 )
 
+/*
+
+Calculates multiple of n which are less than m
+
+*/
 func Multiple(n, m int) int {
 	var sum, mul int
 
@@ -14,6 +25,8 @@ func Multiple(n, m int) int {
 
 	return sum
 }
+
+// Add all multiples of 3 and 5 and substract common multiples (15)
 
 func Sum(n int) int {
 
